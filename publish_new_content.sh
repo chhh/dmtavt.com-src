c079c69ada742ef7416224bb8053a6f90dcb31f7
Dmitry Avtonomov <dmt.avt@gmail.com>
2020-01-26 12:26:46 -0800

    Almost complete publish script

--- publish_new_content.sh
+++ publish_new_content.sh
@@ -1,30 +1,57 @@
 #!/bin/bash
 
-allowed_dir="chhh.github.io"
+source_dir="dmtavt.com-src"
+publish_dir_rel="../chhh.github.io"
+publish_branch="master"
 
-if [ ${PWD##*/} != "src" ]; then
-	 printf"\n\nYou must be in 'src' direcotry to execute this script.\n"
+cur_dir=${PWD##*/}
+
+# Check that we're in the right directory to begin with
+if [ "$cur_dir" != "$source_dir" ]; then
+	printf"\n\nYou must be in '$source_dir' direcotry to execute this script.\n"
 	exit 1
 fi
 
-cd ..
-cur_dir=${PWD##*/}
-
-printf "\nUpper level dir: %s\n" $cur_dir
-if [ $cur_dir == "$allowed_dir" ]; then
-	printf "\n\nRemoving old content:\n"
-	ls | grep -v src 
-	ls | grep -v src | xargs rm -rf
-	cd src
-	printf "\n\nBuilding new content...\n"
-	rm -rf public
-	hugo
-	printf "\n\nCopying new content:\n"
-	ls public/*
-	cp -r public/* ..
-	printf "\n\nRemoving temporary files.\n"
-	rm -rf public
-else
-	printf "\n\nYou must be in $allowed_dir/src to execute this script\n"
+# Check that publish dir exists
+if [ ! -d "$publish_dir_rel" ]; then
+	printf"\n\nPublishing dir does not exist: '$publish_dir_rel'.\n"
+	exit 1
 fi
 
+printf "\n\nRemoving old content:\n"
+rm -rf public
+
+printf "\n\nBuilding new content...\n"
+hugo
+
+
+# Offer to publish changes
+read -n 1 -p "Would you like to publish changes? (y/n/c)" ans;
+printf "\n\n"
+
+case $ans in
+    y|Y)
+		cd $publish_dir_rel
+		CWD=$(pwd)
+		printf "Working in $CWD"
+		WORKTREE=$publish_dir_rel
+		CHANGED=$(git --work-tree=${WORKTREE} status --porcelain)
+		if [ -n "${CHANGED}" ]; then
+			printf 'Publishing repo has uncommitted changes'
+		else
+			printf 'Publishing repo is clean, can publish new content'
+		fi
+		printf "Checking out $publish_branch, deleting old stuff, pushing new content upstream"
+		git checkout $publish_branch
+		git rm -rf .;
+		git add -A
+		git commit -m "Contents update"
+		git push
+		exit;;
+    n|N)
+        printf "\n\nNot publishing changes, exiting."
+        exit;;
+    *)
+		printf "\n\nNot publishing changes, exiting."
+        exit;;
+esac
